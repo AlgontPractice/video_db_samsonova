13a5ed324367d88e4005f122f9d175d4756f4c89
praktika <tagapur44@gmail.com>
2022-07-08 10:17:25 +0300

    реализованно раздельное подключение к базе для запросов

--- record_bd.py
+++ record_bd.py
@@ -1,49 +1,62 @@
 import psycopg2
-import datetime
-
-con = psycopg2.connect(
-    database="Record_bd",
-    user="postgres",
-    password="12345",
-    host="localhost",
-    port="5432"
-)
-
+from psycopg2 import pool
 
 def insert_record(channel, record_type, record, record_path, datetime_start, datetime_stop, record_length,
                   record_extension, snapshot_path):
-    cur = con.cursor()
+    global postgresql_pool
     try:
-        cur.execute("INSERT INTO record_info (id_channel, record_type, id_record, record_path, datetime_start, "
-                    "datetime_stop, record_length, record_extension, snapshot_path) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s)",
-                    (channel, record_type, record, record_path, datetime_start, datetime_stop, record_length,
-                     record_extension, snapshot_path))
-    except psycopg2.DatabaseError as err:
-        print("Error: ", err)
-    else:
-        con.commit()
-    con.close()
-
+        postgresql_pool = psycopg2.pool.ThreadedConnectionPool(1, 20,
+                                                               database="Record_bd",
+                                                               user="postgres",
+                                                               password="12345",
+                                                               host="localhost",
+                                                               port="5432"
+                                                               )
+        con = postgresql_pool.getconn()
+        if con:
+            cur = con.cursor()
+            sql = "INSERT INTO record_info (id_channel, record_type, id_record, record_path, datetime_start, " \
+                  "datetime_stop, record_length, record_extension, snapshot_path) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s) "
+            try:
+                cur.execute(sql, (channel, record_type, record, record_path, datetime_start, datetime_stop, record_length, record_extension, snapshot_path))
+            except psycopg2.DatabaseError as err:
+                print("Error: ", err)
+            finally:
+                con.commit()
+            cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print("Error", error)
+    finally:
+        if postgresql_pool:
+            postgresql_pool.closeall()
 
 def select_record(dt_start, dt_stop):
-    global record
-    cur = con.cursor()
-    sql = "SELECT * FROM record_info WHERE datetime_start > %s AND datetime_stop < %s"
+    global record, postgresql_pool
     try:
-        cur.execute(sql, (dt_start, dt_stop))
-        record = cur.fetchall()  # возвращает все строки
-        print(record)
-    except psycopg2.DatabaseError as err:
-        print("Error: ", err)
-    else:
-        con.commit()
-    con.close()
-    if record is not None:
-        return record
-
-
-start = datetime.datetime(2022, 7, 6, 11, 30, 40)
-stop = datetime.datetime(2022, 7, 6, 15, 22, 56)
-select_record(start, stop)
-# now = datetime.datetime.now()
-# insert_record(1, 'con', 1, '/file', now, now, 12, 'mp4', '/file')
+        postgresql_pool = psycopg2.pool.ThreadedConnectionPool(1, 20,
+                                                               database="Record_bd",
+                                                               user="postgres",
+                                                               password="12345",
+                                                               host="localhost",
+                                                               port="5432"
+                                                               )
+        con = postgresql_pool.getconn()
+        if con:
+            cur = con.cursor()
+            sql = "SELECT * FROM record_info WHERE datetime_start > %s AND datetime_stop < %s"
+            try:
+                cur.execute(sql, (dt_start, dt_stop))
+                record = cur.fetchall()  # возвращает все строки
+                print(record)
+            except psycopg2.DatabaseError as err:
+                print("Error: ", err)
+            finally:
+                con.commit()
+            cur.close()
+            if record is not None:
+                return record
+    except (Exception, psycopg2.DatabaseError) as error:
+        print("Error", error)
+    finally:
+        if postgresql_pool:
+            postgresql_pool.closeall()
