ea45f4fa0a914c61b751acc80ed0dae99f1b5fd2
praktika <tagapur44@gmail.com>
2022-07-15 09:19:51 +0300

    изменен тип связи с бд

--- record_db.py
+++ record_db.py
@@ -85,4 +85,4 @@ def select_record(datetime_start: str, datetime_stop: str) -> list:
 
 
 if __name__ == '__main__':
-    app.run(port=1234, host='0.       0.0.0')
+    app.run(port=1234, host='0.0.0.0')
